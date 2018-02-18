e74515fc05e97facb9054f7e61bbc504d2f0e090
Andrew Tuzson <andrew@evolempirecreative.com>
2018-02-18 09:56:51 -0600

    - Added two additional view controllers - Added segue from vc1 to v2

--- AppSwoosh/ViewController.swift
+++ AppSwoosh/ViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var swooshLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
 
 
